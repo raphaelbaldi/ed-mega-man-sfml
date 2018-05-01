8daa67346890b6a90dc72c5213e61587dfed9452
Raphael Baldi <raphaelbaldi@gmail.com>
2018-05-01 20:53:44 -0300

    Initial commit after getting rid o LFS

---
+++ content/level/bombman/bombman_map_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="bombman_map_tiles" tilewidth="16" tileheight="16" tilecount="45" columns="9">
+ <image source="bombman_map_tiles.png" trans="63b4ff" width="144" height="80"/>
+</tileset>
